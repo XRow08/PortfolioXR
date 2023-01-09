c858d2616bddb2ea72714a14bb1671b20e3ee72e
XRow08 <murilloaugustoe081@gmail.com>
2023-01-09 09:29:16 -0300

    fix

--- script.js
+++ script.js
@@ -1,55 +1,62 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-     $('.scroll-up-btn').click(function(){
-         $('html').animate({scrollTop: 0});
-     });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass('sticky');
+    } else {
+      $('.navbar').removeClass('sticky');
+    }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
+  });
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+  });
+  var typed = new Typed('.typing', {
+    strings: ['Designs', 'Sistemas', 'WebSites', 'LandingPages'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  var typed = new Typed('.typing-2', {
+    strings: ['Dev FullStack', 'Programador', 'Designer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-     var typed = new Typed(".typing", {
-         strings:["Designs", "Sistemas", "WebSites", "LandingPages"],
-         typeSpeed:100,
-         backSpeed:60,
-         loop:true
-     });
-     var typed = new Typed(".typing-2", {
-        strings:["Dev FullStack", "Programador", "Designer", "Freelancer"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
-    });
+  $('.menu-btn, .menu a').click(function () {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
+  });
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPauser: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
 
-    $('.menu-btn, .menu a').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-    $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:false
-            }
-        }
-    });
+VanillaTilt.init(document.querySelectorAll('.card'), {
+  max: 25,
+  speed: 400,
+  glare: true,
+  scale: 1.1,
+  'max-glare': 1,
 });
